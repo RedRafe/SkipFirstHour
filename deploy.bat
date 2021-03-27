d494bbaca05d6887a28a37e285162e0a5ada734f
Tekin Ozbek <tekin@tekinozbek.net>
2021-03-27 00:53:37 -0400

    Fix deployment script by moving items into a folder before zipping

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,9 @@
-tar -c -a -f SkipFirstHour.zip^
-	info.json^
-	LICENSE^
-	control.lua^
-	config.lua^
+mkdir SkipFirstHour
+copy info.json SkipFirstHour\info.json
+copy LICENSE SkipFirstHour\LICENSE
+copy control.lua SkipFirstHour\control.lua
+copy config.lua SkipFirstHour\config.lua
+
+tar -c -a -f SkipFirstHour.zip SkipFirstHour
+	
+rmdir /S /Q SkipFirstHour
